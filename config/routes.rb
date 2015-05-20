a82450877e738981a743c7d9633c744fadc66497
Henry Paulino <henrypl360@gmail.com>
2015-05-20 18:37:15 -0400

    Added user profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+
   devise_for :users, controllers: {registrations: 'registrations'}
   root 'pages#home'
+
+  resources :users, only: [:show]
 
 
   # The priority is based upon order of creation: first created -> highest priority.
